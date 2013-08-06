1be71d14b3b13e262f976bd599c47bc6d0bb605d
zhilong0 <zhilong0.liang@gmail.com>
2013-08-06 17:50:53 +0800

    Main activity development

--- modules/android-client/src/com/df/android/Main.java
+++ modules/android-client/src/com/df/android/Main.java
@@ -1,19 +1,63 @@
 package com.df.android;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.TextView;
 
-public class Main extends Activity {
-
+public class Main extends Activity { 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        super.onCreate(savedInstanceState); 
+//        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         setContentView(R.layout.main);
 
+        final SlidingPaneLayout spl = (SlidingPaneLayout)findViewById(R.id.sliding_pane_layout);
+        ((TextView)findViewById(R.id.lstOrder)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) { 
+				Log.i("Main", "Panel clicked");
+				spl.smoothSlideClosed();
+			} 
+        	
+        });
+        spl.openPane();
+        
+        GridView gvMenu = (GridView) findViewById(R.id.gvMenu);
+        List<MenuItem> menus = new ArrayList<MenuItem>();
+		try {
+	        String imageFiles[] = this.getAssets().list("cache/image");
+	        for (int i = 0; i < imageFiles.length; ++i) {
+	        	Log.i("Main", "found " + imageFiles[i]);
+	            menus.add(new MenuItem("清蒸鲈鱼", "cache/image/" + imageFiles[i]));
+	        }
+		} catch (IOException e) { 
+			e.printStackTrace();
+		}
+        gvMenu.setAdapter(new MenuAdapter(this, menus));
+        
+        gvMenu.setOnItemClickListener(new OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+//                Toast.makeText(Main.this, mAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+//        
 //        SlidingUpPanelLayout layout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
-//        layout.setShadowDrawable(getResources().getDrawable(R.drawable.above_shadow));
+////        layout.setShadowDrawable(getResources().getDrawable(R.drawable.above_shadow));
 //        layout.setPanelSlideListener(new PanelSlideListener() {
 //
 //            @Override
@@ -41,7 +85,7 @@ public class Main extends Activity {
 //
 //            }
 //        });
-//        TextView t = (TextView) findViewById(R.id.brought_by);
-//        t.setMovementMethod(LinkMovementMethod.getInstance());
+////        TextView t = (TextView) findViewById(R.id.brought_by);
+////        t.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
---
+++ modules/android-client/src/com/df/android/MenuItem.java
@@ -0,0 +1,20 @@
+package com.df.android;
+
+public class MenuItem {
+	private String name;
+	private String image;
+	
+	public MenuItem(String name, String image) {
+		this.name = name;
+		this.image = image;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
+}
