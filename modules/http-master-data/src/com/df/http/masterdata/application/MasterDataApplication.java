1007283a35cf7aedfcb2441376b6149f71a78011
sala223 <sala223@msn.com>
2013-09-02 19:46:21 +0800

    Integrate resteasy with spring to enable jax-rs

---
+++ modules/http-master-data/src/com/df/http/masterdata/application/MasterDataApplication.java
@@ -0,0 +1,9 @@
+package com.df.http.masterdata.application;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class MasterDataApplication extends Application {
+
+}
