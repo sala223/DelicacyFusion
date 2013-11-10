dd5a36623fac897e023813492ef22cfa2557075b
sala223 <sala223@msn.com>
2013-11-10 18:09:01 +0800

    Add getItemImage method in ItemResource

--- modules/android/android-http-client-sdk/src/main/com/df/client/rs/resource/ItemResource.java
+++ modules/android/android-http-client-sdk/src/main/com/df/client/rs/resource/ItemResource.java
@@ -1,13 +1,17 @@
 package com.df.client.rs.resource;
 
+import java.io.InputStream;
+
 import com.df.client.rs.model.Item;
 
 public interface ItemResource extends Resource {
 
-    public Item[] getItems();
+	public Item[] getItems();
 
-    public Item[] getFoods();
+	public Item[] getFoods();
 
-    public Item[] getFoodsByCategory(String categoryCode);
+	public Item[] getFoodsByCategory(String categoryCode);
+
+	public InputStream getItemImage(Item item, String imageId);
 
 }
