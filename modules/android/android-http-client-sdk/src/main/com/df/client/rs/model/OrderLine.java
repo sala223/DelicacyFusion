e71832f9e35dffbd761d3cd7868fdbee55bcf84b
zhilong0 <zhilong0.liang@gmail.com>
2013-11-10 16:45:06 +0800

    change item to itemCode

--- modules/android/android-http-client-sdk/src/main/com/df/client/rs/model/OrderLine.java
+++ modules/android/android-http-client-sdk/src/main/com/df/client/rs/model/OrderLine.java
@@ -5,149 +5,155 @@ import java.math.BigDecimal;
 
 public class OrderLine implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private int lineNumber;
+	private int lineNumber;
 
-//    private String itemCode;
-    private Item item;
+	private String itemCode;
 
-    private String roomCode;
+	// private Item item;
 
-    private String tableNumber;
+	private String roomCode;
 
-    private float price;
+	private String tableNumber;
 
-    private float promotionPrice;
+	private float price;
 
-    private BigDecimal discount;
+	private float promotionPrice;
 
-    private int quantity;
+	private BigDecimal discount;
 
-    private BigDecimal totalPayment;
+	private int quantity;
 
-    private BigDecimal promotionTotalPayment;
+	private BigDecimal totalPayment;
 
-    private String comment;
+	private BigDecimal promotionTotalPayment;
 
-    private Long promotionId;
+	private String comment;
 
-    private String promotionName;
+	private Long promotionId;
 
-    OrderLine() {
-    }
+	private String promotionName;
 
-    public OrderLine(Item item) {
-	this(item, 1);
-    }
-    
-    public OrderLine(Item item, int quantity) {
-	this.item = item;
-	this.quantity = quantity;
-    }
-
-    public Item getItem() {
-	return item;
-    }
-
-    public String getRoomCode() {
-	return roomCode;
-    }
-
-    public void setRoomCode(String roomCode) {
-	this.roomCode = roomCode;
-    }
-
-    public float getPrice() {
-	return price;
-    }
-
-    public void setPrice(float price) {
-	this.price = price;
-    }
-
-    public int getQuantity() {
-	return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-	this.quantity = quantity;
-    }
-
-    public BigDecimal getTotalPayment() {
-	return totalPayment;
-    }
-
-    public String getComment() {
-	return comment;
-    }
-
-    public void setComment(String comment) {
-	this.comment = comment;
-    }
-
-    public int getLineNumber() {
-	return lineNumber;
-    }
-
-    public void setLineNumber(int lineNumber) {
-	this.lineNumber = lineNumber;
-    }
-
-    public long getPromotionId() {
-	return promotionId;
-    }
-
-    public void setPromotionId(long promotionId) {
-	this.promotionId = promotionId;
-    }
-
-    public BigDecimal getDiscount() {
-	return discount;
-    }
-
-    public String getPromotionName() {
-	return promotionName;
-    }
-
-    public void setPromotionName(String promotionName) {
-	this.promotionName = promotionName;
-    }
-
-    public float getPromotionPrice() {
-	return promotionPrice;
-    }
-
-    public void setPromotionPrice(float promotionPrice) {
-	this.promotionPrice = promotionPrice;
-    }
-
-    public String getTableNumber() {
-	return tableNumber;
-    }
-
-    public void setTableNumber(String tableNumber) {
-	this.tableNumber = tableNumber;
-    }
-
-    public BigDecimal getPromotionTotalPayment() {
-	return promotionTotalPayment;
-    }
-
-    public boolean hasPromotion() {
-	return this.promotionId != null && promotionId.longValue() != 1l;
-    }
-
-    public void calcuatePayment() {
-	this.totalPayment = new BigDecimal(this.price).multiply(new BigDecimal(quantity));
-	if (this.hasPromotion()) {
-	    this.promotionTotalPayment = new BigDecimal(this.promotionPrice).multiply(new BigDecimal(quantity));
-	    this.discount = this.totalPayment.subtract(this.promotionTotalPayment);
+	OrderLine() {
 	}
-    }
-    
+
+	public OrderLine(String itemCode) {
+		this(itemCode, 1);
+	}
+
+	public OrderLine(String itemCode, int quantity) {
+		this.itemCode = itemCode;
+		this.quantity = quantity;
+	}
+
+	public String getRoomCode() {
+		return roomCode;
+	}
+
+	public void setRoomCode(String roomCode) {
+		this.roomCode = roomCode;
+	}
+
+	public String getItemCode() {
+		return itemCode;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getTotalPayment() {
+		return totalPayment;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	public void setLineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+
+	public long getPromotionId() {
+		return promotionId;
+	}
+
+	public void setPromotionId(long promotionId) {
+		this.promotionId = promotionId;
+	}
+
+	public BigDecimal getDiscount() {
+		return discount;
+	}
+
+	public String getPromotionName() {
+		return promotionName;
+	}
+
+	public void setPromotionName(String promotionName) {
+		this.promotionName = promotionName;
+	}
+
+	public float getPromotionPrice() {
+		return promotionPrice;
+	}
+
+	public void setPromotionPrice(float promotionPrice) {
+		this.promotionPrice = promotionPrice;
+	}
+
+	public String getTableNumber() {
+		return tableNumber;
+	}
+
+	public void setTableNumber(String tableNumber) {
+		this.tableNumber = tableNumber;
+	}
+
+	public BigDecimal getPromotionTotalPayment() {
+		return promotionTotalPayment;
+	}
+
+	public boolean hasPromotion() {
+		return this.promotionId != null && promotionId.longValue() != 1l;
+	}
+
+	public void calcuatePayment() {
+		this.totalPayment = new BigDecimal(this.price).multiply(new BigDecimal(
+				quantity));
+		if (this.hasPromotion()) {
+			this.promotionTotalPayment = new BigDecimal(this.promotionPrice)
+					.multiply(new BigDecimal(quantity));
+			this.discount = this.totalPayment
+					.subtract(this.promotionTotalPayment);
+		}
+	}
+
 	@Override
 	public boolean equals(Object o) {
-		return o != null && tableNumber.equals(((OrderLine)o).getTableNumber()) && item.equals(((OrderLine)o).getItem());
+		return o != null
+				&& tableNumber.equals(((OrderLine) o).getTableNumber())
+				&& itemCode.equals(((OrderLine) o).getItemCode());
 	}
 }
