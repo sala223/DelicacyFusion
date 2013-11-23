7d156a7373a2f2e2eb8baf1fbb0291bf2a52a7a7
zhilong0 <zhilong0.liang@gmail.com>
2013-11-23 20:13:11 +0800

    back from svn

---
+++ modules/android/android-client/test/com/df/android/service/test/ServiceTest.java
@@ -0,0 +1,14 @@
+package com.df.android.service.test;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class ServiceTest extends TestCase {
+	@Test
+	public void testCreate() {
+//		Service svc = Service.newService("http://192.168.0.104:8080/m-console/rs");
+//		Store[] stores = svc.getStores("test");
+	}
+	
+}
