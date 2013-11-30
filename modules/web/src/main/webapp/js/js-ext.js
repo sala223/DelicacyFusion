12ef78a58b6bda24fef31a8ad36b6435cf280c86
mcfu <fuyubo@gmail.com>
2013-11-30 19:56:21 +0800

    tenant landing page

--- modules/web/src/main/webapp/js/js-ext.js
+++ modules/web/src/main/webapp/js/js-ext.js
@@ -16,3 +16,39 @@ window.getQueryParams=function() {
 
     return params;
 };
+
+window.nextValue = function(currValue){
+    var intValue = parseInt(currValue,10);
+    if(typeof(currValue)==='number' || !isNaN(intValue)){
+        return currValue + 1;
+    }
+
+
+    var v = currValue.toUpperCase(), i = 0 ,cc, cr = 1,
+        vc = [];
+
+    while(i<v.length){
+        vc.push(v.charCodeAt(i));
+        i++;
+    }
+
+    i = v.length - 1;
+    while( cr > 0 ){
+        cc = vc[i] + cr;
+        cr = 0;
+        if(cc === 91){
+            cc = 65;
+            cr = 1;
+        } else if(cc === 58) {
+            cc = 48
+            cr = 1;
+        }
+
+        vc[i] = cc;
+        i--;
+    }
+
+    return vc.map(function(c){
+        return String.fromCharCode(c);
+    }).join('');
+};
